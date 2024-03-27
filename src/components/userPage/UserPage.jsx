d595f1b19e1d79d27badb960365a08615050a7ae
Paolo <lupo.paolo96@gmail.com>
2024-03-27 12:14:41 +0100

    Added assets to UserPage component

--- src/components/userPage/UserPage.jsx
+++ src/components/userPage/UserPage.jsx
@@ -10,31 +10,36 @@ export function UserPage() {
       <div className="cards_collection">
         <div className="cards_collection_container">
           <div className="user_card_container">
-            <img src="" alt="" />
+            <div className="icon_img_container">
+              <img
+                src="src\assets\paoloLupoAssets\user_avatar.png"
+                alt="user_avatar"
+              />
+            </div>
             <p>Personalizza</p>
           </div>
           <div className="user_card_container">
-            <img src="" alt="" />
+            <div className="icon_img_container"><img src="src\assets\paoloLupoAssets\myOrders_icon.avif" alt="" /></div>
             <p>I miei Ordini</p>
           </div>
         </div>
         <div className="cards_collection_container">
           <div className="user_card_container">
-            <img src="" alt="" />
+            <div className="icon_img_container"><img src="src\assets\paoloLupoAssets\pixel-treasure-chest-vector-28943116.webp" alt="" /></div>
             <p>Wishlist</p>
           </div>
           <div className="user_card_container">
-            <img src="" alt="" />
+            <div className="icon_img_container"><img src="src\assets\paoloLupoAssets\shield_privacy_icon.png" alt="" /></div>
             <p>Privacy e Sicurezza</p>
           </div>
         </div>
         <div className="cards_collection_container">
           <div className="user_card_container">
-            <img src="" alt="" />
+            <siv className="icon_img_container"><img src="src\assets\paoloLupoAssets\newsletter_icon.jpg" alt="" /></siv>
             <p>Newsletter</p>
           </div>
           <div className="user_card_container">
-            <img src="" alt="" />
+            <div className="icon_img_container"><img src="src\assets\paoloLupoAssets\exitDoor_icon.jpg" alt="" /></div>
             <p>Exit</p>
           </div>
         </div>
