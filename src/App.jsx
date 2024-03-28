9b8f26b274e0355dec825c61d611dfe65c6440ca
ariannagiaccari <ariggiaccari@gmail.com>
2024-03-28 23:08:11 +0100

    wishlist

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Wishlist } from "./Wishlist";
+
 export function App() {
 
   return (
     <>
+    <Wishlist />
     </>
   )
 }
---
+++ src/Wishlist.jsx
@@ -0,0 +1,38 @@
+import './wishlist.css'
+export function Wishlist() {
+
+    return (
+
+        <div className="wishlistBackground">
+            <img className='wishlistBackgroundImg' src='src\assets\loghi\ariGiaccariAssets\sfondo_wishlist.png' alt="" />
+            <div className='wishlistContainer'>
+
+                <div className="upperWishlist">
+                    <img style={{ width: '100px', height: '100%' }} src="src\assets\loghi\ariGiaccariAssets\logo-removebg-preview.png" alt="" />
+                    <h1>LISTA DESIDERI</h1>
+                    <img style={{ width: '70px', height: '100%', paddingTop: '10px' }} src="src\assets\loghi\ariGiaccariAssets\cart_full-removebg-preview.png" alt="" />
+                </div>
+
+                <div className="favContainer">
+                    <img style={{ width: '200px', height: '100%' }} src="src\assets\loghi\ariGiaccariAssets\sfondo_wishlist.png" alt="" />
+                    <div className="favProdName">
+                        <p>nome del prodotto</p>
+                        <p>Hello World</p>
+                    </div>
+                    <div className="favProdPrice">
+                        <p>prezzo</p>
+                        <p>£12,99</p>
+                    </div>
+                    <div className="favProdStatus">disponibile?</div>
+                    <div className="addRemoveBtns">
+                        <button>aggiungi al carrello</button> 
+                        <button>rimuovi</button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
+    )
+}
