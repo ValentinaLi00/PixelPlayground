f64edbe6f0ddda8492508f19b59d1cd48dc3e336
Paolo <lupo.paolo96@gmail.com>
2024-03-28 17:05:42 +0100

    Updated Cart component

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -3,11 +3,10 @@ import "./cart.css";
 export function Cart() {
   return (
     <div className="cart-container">
-      <h2 className="cart-title">Il tuo carrello</h2>
+      <h2 className="cart-title">Carrello</h2>
       <div className="cart-items">
-        
         {/* Mappare qui gli articoli nel carrello */}
-        
+
         {/* Esempio di struttura per un singolo elemento del carrello */}
         {/* Sostituire con dati reali */}
         <div className="cart-item">
@@ -17,22 +16,28 @@ export function Cart() {
           <div className="cart-item-details">
             <p className="cart-item-title">Titolo del gioco</p>
             <p className="cart-item-price">Prezzo: $XX.XX</p>
-            {/* Aggiungi pulsanti per modificare o rimuovere l'articolo */}
+            <div className="cart-buttons">
+              <button>Aggiungi</button>
+              <button>Rimuovi</button>
+            </div>
           </div>
         </div>
-        {/* Fine esempio di elemento del carrello */}
-
       </div>
-      <div className="cart-total">
-        <p className="cart-total-text">Totale:</p>
-        <p className="cart-total-amount">$XX.XX</p>
-      </div>
-      <div className="cart-buttons">
-        {/* Aggiungi qui pulsanti per il checkout o per tornare allo shopping */}
-        <button className="checkout-button">Vai al checkout</button>
-        <button className="continue-shopping-button">
-          Continua lo shopping
-        </button>
+      <div>
+        <h2 className="cart-title">Riepilogo</h2>
+        <div>
+          <ul>{/* map lista giochi nel carrello */}</ul>
+          <div className="cart-total">
+            <p className="cart-total-text">Totale:</p>
+            <p className="cart-total-amount">$XX.XX</p>
+          </div>
+          <div className="cart-buttons">
+            <button className="checkout-button">Vai al checkout</button>
+            <button className="continue-shopping-button">
+              Continua lo shopping
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
