697c13d113ca4dc168f43a8e9e297f8a276d5881
ariannagiaccari <ariggiaccari@gmail.com>
2024-04-06 15:12:16 +0200

    userPage

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export function App() {
   return (
     <div>
       <UserPage />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/userPage/UserPage.jsx
+++ src/components/userPage/UserPage.jsx
@@ -1,49 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import "./userPage.css";
 
 export function UserPage() {
+  const [editing, setEditing] = useState(false);
+  const [email, setEmail] = useState('');
+
+  const [editingPsw, setEditingPsw] = useState(false);
+  const [password, setPassword] = useState('');
+
   return (
-    <div className="UserPage_component">
-      {/* <div className="userPage_logo_container">
-        <img src="src\assets\loghi\logo-removebg-preview.png" alt="" />
-      </div> */}
-      <div className="cards_collection">
-        <div className="cards_collection_container">
-          <div className="user_card_container">
-            <div className="icon_img_container">
-              <img
-                src="src\assets\paoloLupoAssets\user_avatar.png"
-                alt="user_avatar"
-              />
+    <>
+      <div className="userIcon">
+        <p>user's name</p>
+        <img src="" alt="user_icon" />
+      </div>
+      <div className="wholeUserPage">
+        <div className="leftUserPart">
+          <div className="account">
+            <p>ACCOUNT</p>
+            <div className="modifyAcc" >
+              {editing ? (
+                <div>
+                  <span>e-mail:</span><input className="inputUserPage" type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} /> <img onClick={() => setEditing(false)} style={{ width: '15px', cursor: 'pointer', height: '100%', transform: 'rotate(40deg)' }} src="src\assets\paoloLupoAssets\pencil_pxl-removebg-preview.png" alt="modify_email" />
+                </div>
+              ) : (
+                <div>
+                  <span>e-mail: jessicarossi@gmail.com </span> <img onClick={() => setEditing(true)} style={{ width: '15px', cursor: 'pointer', height: '100%', transform: 'rotate(40deg)' }} src="src\assets\paoloLupoAssets\pencil_pxl-removebg-preview.png" alt="modify_email" />
+                </div>
+              )}
+              {editingPsw ? (
+                <>
+                  <div>
+                    <span>password:</span><input className="inputUserPage" type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} /> <img onClick={() => setEditingPsw(false)} style={{ width: '15px', cursor: 'pointer', height: '100%', transform: 'rotate(40deg)' }} src="src\assets\paoloLupoAssets\pencil_pxl-removebg-preview.png" alt="modify_password" />
+                  </div>
+                  <p>cancella account</p>
+                </>) : (
+                <>
+                  <div>
+                    <span>password:  ************ </span> <img onClick={() => setEditingPsw(true)} style={{ width: '15px', cursor: 'pointer', height: '100%', transform: 'rotate(40deg)' }} src="src\assets\paoloLupoAssets\pencil_pxl-removebg-preview.png" alt="modify-psw" />
+                  </div>
+                  <p>cancella account</p>
+                </>
+              )}
+
             </div>
-            <p>Personalizza</p>
+
           </div>
-          <div className="user_card_container">
-            <div className="icon_img_container"><img src="src\assets\paoloLupoAssets\myOrders_icon.avif" alt="" /></div>
-            <p>I miei Ordini</p>
-          </div>
-        </div>
-        <div className="cards_collection_container">
-          <div className="user_card_container">
-            <div className="icon_img_container"><img src="src\assets\paoloLupoAssets\chest_wishlist_icon.png" alt="" /></div>
-            <p>Wishlist</p>
-          </div>
-          <div className="user_card_container">
-            <div className="icon_img_container"><img src="src\assets\paoloLupoAssets\shield_privacy_icon.png" alt="" /></div>
-            <p>Privacy e Sicurezza</p>
+          <div className="orders">
+            <p>I MIEI ORDINI</p>
+            <div className="ordersList">
+              <p>ordine:</p>
+              <p>data: </p>
+              <p>mostra altro</p>
+            </div>
           </div>
         </div>
-        <div className="cards_collection_container">
-          <div className="user_card_container">
-            <siv className="icon_img_container"><img src="src\assets\paoloLupoAssets\newsletter_icon.jpg" alt="" /></siv>
-            <p>Newsletter</p>
+        <div className="rightUserPart">
+          <div className="newsletter">
+            <p>NEWSLETTER</p>
+            <div className="subscribe">
+              <p>iscriviti per rimanere aggiornato</p>
+              <img style={{width: '50px'}} src="src\assets\paoloLupoAssets\newsletter-removebg-preview.png" alt="" />
+              <button>iscriviti</button>
+            </div>
           </div>
-          <div className="user_card_container">
-            <div className="icon_img_container"><img src="src\assets\paoloLupoAssets\exitDoor_icon.jpg" alt="" /></div>
-            <p>Exit</p>
+          <div classname='wishlistAndExit'>
+            <div className="wishlist">
+              <p>WISHLIST</p>
+              <img src="" alt="heart_wishlist_icon" />
+            </div>
+            <div className="exit">
+              <p>EXIT</p>
+              <img src="src\assets\paoloLupoAssets\exitDoor_icon.jpg" alt="exit_icon" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
