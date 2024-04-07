00d9264bafa9d2867f3718a605452cfd68fb916b
valentina Li <valentinalidev@gmail.com>
2024-04-07 18:47:07 +0200

    modified slider home

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 
 import { AboutUs } from "./components/about_us/AboutUs";
 import { Catalogue } from "./components/catalogue/products/Catalogue";
+import { DiscountedGames } from "./components/home/DiscountedGames";
 import { LoginRegistration } from "./components/login/LoginRegistration";
 
 export function App() {
 
   return (
     <>
-      <LoginRegistration />
-      <Catalogue />
-      <AboutUs />
+     <DiscountedGames/>
     </>
   )
 }
