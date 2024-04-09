342a54183a02f8c3b5f520535a0f9d8b6b624702
Paolo <lupo.paolo96@gmail.com>
2024-04-09 12:41:48 +0200

    Updated Userpage component's style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { UploadAvatar } from "./components/UploadAvatar/UploadAvatar";
-import { Footer } from "./components/footer/Footer";
 import { Product } from "./components/product/Product";
 import { UserPage } from "./components/userPage/UserPage";
+import { UploadAvatar } from "./components/UploadAvatar/UploadAvatar";
+import { Footer } from "./components/footer/Footer";
 
 export function App() {
   return (
     <div>
-      <Product />
-      {/* <UserPage /> */}
+      {/* <Product /> */}
+      <UserPage />
       <Footer />
       {/* <UploadAvatar /> */}
     </div>
