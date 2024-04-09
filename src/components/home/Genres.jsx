b3709f5406cddec2abf951e3ca4c642fb1045dca
valentina Li <valentinalidev@gmail.com>
2024-04-09 20:23:01 +0200

    modified genre and added photos

--- src/components/home/Genres.jsx
+++ src/components/home/Genres.jsx
@@ -13,43 +13,55 @@ export function Genres() {
 
                 <div className={classes.containerCards}>
 
-                    <div className={classes.card} >
-                        <img src="src\assets\valentinaLiAssets\home\omen.png" alt="" className={classes.character}/>
-                        <h3>FPS</h3>
-                    </div>
-                    
-                    <div className={classes.card} >
-                        <img src="src\assets\valentinaLiAssets\home\omen.png" alt="" className={classes.character}/>
+                    <div className={`${classes.card} ${classes.action} `}>
+                        <img src="src\assets\valentinaLiAssets\home\genres\action_figure.png" alt="" className={`${classes.character} ${classes.figure_action}`} />
+                        <div className={classes.ribbon}>Action</div> 
                     </div>
 
-                    <div className={classes.card} >
-                        <img src="src\assets\valentinaLiAssets\home\omen.png" alt="" className={classes.character}/>
+                    <div className={`${classes.card} ${classes.fighting} `}>
+                        <img src="src\assets\valentinaLiAssets\home\genres\fighting_figure.png" alt="" className={`${classes.character} ${classes.figure_fighting}`} />
+                        <div className={classes.ribbon}>Fighting</div> 
                     </div>
 
-                    <div className={classes.card} >
-                        <img src="src\assets\valentinaLiAssets\home\omen.png" alt="" className={classes.character}/>
+                    <div className={`${classes.card} ${classes.adventures} `}>
+                        <img src="src\assets\valentinaLiAssets\home\genres\adventure_figure.png" alt="" className={`${classes.character} ${classes.figure_adventures}`} />
+                        <div className={classes.ribbon}>Adventure</div> 
                     </div>
 
-                    <div className={classes.card} >
-                        <img src="src\assets\valentinaLiAssets\home\omen.png" alt="" className={classes.character}/>
+                    <div className={`${classes.card} ${classes.multiplayer} `}>
+                        <img src="src\assets\valentinaLiAssets\home\genres\multiplayer_figure.png" alt="" className={`${classes.character} ${classes.figure_multiplayer}`} />
+                        <div className={classes.ribbon}>Multiplayer</div> 
                     </div>
 
-                    <div className={classes.card} >
-                        <img src="src\assets\valentinaLiAssets\home\omen.png" alt="" className={classes.character}/>
+                    <div className={`${classes.card} ${classes.fps} `}>
+                        <img src="src\assets\valentinaLiAssets\home\genres\fps_figure.webp" alt="" className={`${classes.character} ${classes.figure_fps}`} />
+                        <div className={classes.ribbon}>FPS</div> 
                     </div>
 
-                    <div className={classes.card} >
-                        <img src="src\assets\valentinaLiAssets\home\omen.png" alt="" className={classes.character}/>
+                    <div className={`${classes.card} ${classes.sport} `}>
+                        <img src="src\assets\valentinaLiAssets\home\genres\sport_figure.png" alt="" className={`${classes.character} ${classes.figure_sport}`} />
+                        <div className={classes.ribbon}>Sport</div> 
                     </div>
 
-                    <div className={classes.card} >
-                        <img src="src\assets\valentinaLiAssets\home\omen.png" alt="" className={classes.character}/>
+                    <div className={`${classes.card} ${classes.indie} `}>
+                        <img src="src\assets\valentinaLiAssets\home\genres\indie_figure.png" alt="" className={`${classes.character} ${classes.figure_indie}`} />
+                        <div className={classes.ribbon}>Indie</div> 
                     </div>
 
-                    <div className={classes.card} >
-                        <img src="src\assets\valentinaLiAssets\home\omen.png" alt="" className={classes.character}/>
+                    <div className={`${classes.card} ${classes.racing} `}>
+                        <img src="src\assets\valentinaLiAssets\home\genres\racing_figure.png" alt="" className={`${classes.character} ${classes.figure_racing}`} />
+                        <div className={classes.ribbon}>Racing</div> 
                     </div>
-                    
+
+                    <div className={`${classes.card} ${classes.rpg} `}>
+                        <img src="src\assets\valentinaLiAssets\home\genres\rpg_figure.png" alt="" className={`${classes.character} ${classes.figure_rpg}`} />
+                        <div className={classes.ribbon}>RPG</div> 
+                    </div>
+
+                    <div className={`${classes.card} ${classes.simulator} `}>
+                        <img src="src\assets\valentinaLiAssets\home\genres\simulator_figure.png" alt="" className={`${classes.character} ${classes.figure_simulator}`} />
+                        <div className={classes.ribbon}>Simulator</div> 
+                    </div>
                 </div>
             </div>
         </div>
