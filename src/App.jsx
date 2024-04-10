1c66aee3f600b6d89691058628694bcbb9daa801
Melissa Mastrovincenzo <melissa.mastrovincenzo@gmail.com>
2024-04-10 16:38:19 +0200

    updated component

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
-import { Product } from "./components/product/Product";
-import { UserPage } from "./components/userPage/UserPage";
-import { UploadAvatar } from "./components/UploadAvatar/UploadAvatar";
-import { Footer } from "./components/footer/Footer";
-import { AboutUs } from "./components/about_us/AboutUs";
-import { Catalogue } from "./components/catalogue/products/Catalogue";
-import { DiscountedGames } from "./components/home/DiscountedGames";
-import { Genres } from "./components/home/Genres";
-import { Gratis } from "./components/home/Gratis";
-import { Home } from "./components/home/Home";
-import { Incoming } from "./components/home/Incoming";
-import { LoginRegistration } from "./components/login/LoginRegistration";
-import { Navbar } from "./Navbar";
+import { Cart } from "./components/cart/Cart"
 import "./styles/style_navbar.css"
-import { Cart } from "./components/cart/Cart";
-import { Payment } from "./components/payment/Payment";
+
 
 export function App() {
   return (
     <>
-      {/* <LoginRegistration/>
-      <AboutUs/>
-      <Catalogue/>   */}
-      <Home />
-      <Navbar />
       < Cart />
-      <Payment />
     </>
   )
 }
