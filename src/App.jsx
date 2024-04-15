a8d56217d066da9d64fda5e7bbbdcac1ddeeb5be
ariannagiaccari <ariggiaccari@gmail.com>
2024-04-15 19:53:24 +0200

    mediaQueries

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,31 @@
 import { Home } from "./components/home/Home";
 import "./style/styleGeneral.css";
 import { Product } from "./components/product/Product";
-<<<<<<< HEAD
-import { Wishlist } from "./components/wishlist/Wishlist";
-import { Faq } from "./components/faq/faq";
-import { UserPage } from "./components/userPage/UserPage";
-
-=======
 import { Catalogue } from "./components/catalogue/Catalogue";
 import { Game } from "./components/catalogue/Game";
 import { Navbar } from "./components/navbar/Navbar";
 import { Footer } from "./components/footer/Footer";
->>>>>>> 98a4aefb0610e98d49017023557b0220e58aca07
+import { Faq } from "./components/faq/faq";
+import { Wishlist } from "./components/wishlist/Wishlist";
+import { UserPage } from "./components/userPage/UserPage";
+import { LoginRegistration } from "./components/login/LoginRegistration";
+
 
 export function App() {
   return (
     <>
-<<<<<<< HEAD
-    <UserPage />
-      <Faq />
-      <Wishlist />
-      <Product/>
-      <Home />
-=======
-
+{/* <LoginRegistration /> */}
       {/* <Product/> */}
-
-      <Catalogue/>
+      {/* <Catalogue/> */}
       {/* <Game/> */}
-      {/* <Navbar /> */}
+      <Navbar />
 
-      {/* <Home /> */}
-      {/* <Footer/> */}
->>>>>>> 98a4aefb0610e98d49017023557b0220e58aca07
-    </>
+      <Home />
+   
+      <UserPage />  
+       <Footer/>
+      {/* <Faq />
+      <Wishlist /> */}
+          </>
   );
 }
