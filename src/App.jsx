56f44c0f176a16a0d08a891407b2647a0d94675f
Paolo <lupo.paolo96@gmail.com>
2024-04-15 15:44:56 +0200

    Modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import { Home } from "./components/home/Home";
 import "./style/styleGeneral.css";
 import { Product } from "./components/product/Product";
-<<<<<<< HEAD
-import { Wishlist } from "./components/wishlist/Wishlist";
-import { Faq } from "./components/faq/faq";
-import { UserPage } from "./components/userPage/UserPage";
-
-=======
 import { Catalogue } from "./components/catalogue/Catalogue";
 import { Game } from "./components/catalogue/Game";
 import { Navbar } from "./components/navbar/Navbar";
 import { Footer } from "./components/footer/Footer";
->>>>>>> 98a4aefb0610e98d49017023557b0220e58aca07
 
 export function App() {
   return (
     <>
-<<<<<<< HEAD
-    <UserPage />
-      <Faq />
-      <Wishlist />
-      <Product/>
-      <Home />
-=======
 
       {/* <Product/> */}
 
@@ -32,7 +18,6 @@ export function App() {
 
       {/* <Home /> */}
       {/* <Footer/> */}
->>>>>>> 98a4aefb0610e98d49017023557b0220e58aca07
     </>
   );
 }
