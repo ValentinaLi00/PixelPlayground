e93cfb0e1acb0ab073c43f1a2338dbe18ffe9dc1
valentina Li <valentinalidev@gmail.com>
2024-04-16 15:15:03 +0200

    added gift Card

--- src/components/home/Gratis.jsx
+++ src/components/home/Gratis.jsx
@@ -4,62 +4,62 @@ export function Gratis() {
     return (
         <div className={classes.container}>
             <div className={classes.title}>
-                <h2>GRATUITI</h2>
+                <h2>GIFT CARD</h2>
                 {/* <img src="src\assets\valentinaLiAssets\home\gift.png" alt="" style={{ width: '40px' }} /> */}
             </div>
 
             <div className={classes.containerImg}>
                 <div className={classes.card}>
-                    <img src="src\assets\valentinaLiAssets\home\elden_rings.jpg" alt="" />
+                    <img src="https://gaming-cdn.com/images/products/5537/orig/carta-playstation-network-15eur-15-euros-card-playstation-4-playstation-5-gioco-playstation-store-italia-cover.jpg?v=1697644479" alt="" />
                     <div className={classes.description}>
-                        <p>Elden Rings</p>
-                        <p>30.99€</p>
-                        <div className={classes.crossed_out}></div>
+                        <p>Gift Card</p>
+                        <p>15€</p>
+                        {/* <div className={classes.crossed_out}></div> */}
                     </div>
                 </div>
 
                 <div className={classes.card}>
-                    <img src="src\assets\valentinaLiAssets\home\eiyuden_chronicle.jpg" alt="" />
+                    <img src="https://gaming-cdn.com/images/products/274/616x353/carta-regalo-xbox-live-25eur-zona-euro-eur25-card-xbox-one-xbox-series-x-s-gioco-microsoft-store-europe-cover.jpg?v=1711451805" alt="" />
                     <div className={classes.description}>
-                        <p>Eiyuden chronicle</p>
-                        <p>25.90€</p>
-                        <div className={classes.crossed_out}></div>
+                        <p>Gift Card</p>
+                        <p>25€</p>
+                        {/* <div className={classes.crossed_out}></div> */}
                     </div>
                 </div>
 
                 <div className={classes.card}>
-                    <img src="src\assets\valentinaLiAssets\home\stalker2.jpg" alt="" />
+                    <img src="https://gaming-cdn.com/images/products/808/orig/steam-gift-card-20eur-20-euros-card-pc-gioco-steam-cover.jpg?v=1697644479" alt="" />
                     <div className={classes.description}>
-                        <p>stalker 2</p>
-                        <p>20.90€</p>
-                        <div className={classes.crossed_out}></div>
+                        <p>gift Card</p>
+                        <p>20€</p>
+                        {/* <div className={classes.crossed_out}></div> */}
                     </div>
                 </div>
 
                 <div className={classes.card}>
-                    <img src="src\assets\valentinaLiAssets\home\elden_rings.jpg" alt="" />
+                    <img src="https://gaming-cdn.com/images/products/5533/orig/carta-playstation-network-10eur-10-euros-card-playstation-3-playstation-4-playstation-5-gioco-playstation-store-italia-cover.jpg?v=1666102544" alt="" />
                     <div className={classes.description}>
-                        <p>Elden Rings</p>
-                        <p>30.99€</p>
-                        <div className={classes.crossed_out}></div>
+                        <p>Gift Card</p>
+                        <p>10€</p>
+                        {/* <div className={classes.crossed_out}></div> */}
                     </div>
                 </div>
 
                 <div className={classes.card}>
-                    <img src="src\assets\valentinaLiAssets\home\eiyuden_chronicle.jpg" alt="" />
+                    <img src="https://gaming-cdn.com/images/products/4931/616x353/nintendo-eshop-card-50eur-50-euros-card-switch-gioco-nintendo-eshop-europe-cover.jpg?v=1697644479" alt="" />
                     <div className={classes.description}>
-                        <p>Eiyuden chronicle</p>
-                        <p>25.90€</p>
-                        <div className={classes.crossed_out}></div>
+                        <p>Gift Card</p>
+                        <p>50€</p>
+                        {/* <div className={classes.crossed_out}></div> */}
                     </div>
                 </div>
 
                 <div className={classes.card}>
-                    <img src="src\assets\valentinaLiAssets\home\stalker2.jpg" alt="" />
+                    <img src="https://gaming-cdn.com/images/products/298/616x353/xbox-gift-card-5eur-euro-area-eur5-card-xbox-one-xbox-series-x-s-game-microsoft-store-europe-cover.jpg?v=1711451817" alt="" />
                     <div className={classes.description}>
-                        <p>stalker 2</p>
-                        <p>20.90€</p>
-                        <div className={classes.crossed_out}></div>
+                        <p>Gift Card</p>
+                        <p>5€</p>
+                        {/* <div className={classes.crossed_out}></div> */}
                     </div>
                 </div>
             </div>
