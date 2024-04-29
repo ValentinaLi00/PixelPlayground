9445926dadb15472f5a317b5111e6b0b114b21f6
ariannagiaccari <ariggiaccari@gmail.com>
2024-04-29 17:09:14 +0200

    back to blue

--- src/components/faq/faq.jsx
+++ src/components/faq/faq.jsx
@@ -41,8 +41,6 @@ export function Faq() {
                                     <br />
                                     <div className={selected === i ? 'answer_shown' : 'answer'} >
                                         <p>{item.answer}</p></div>
-
-
                                 </>
                             )
                         })
