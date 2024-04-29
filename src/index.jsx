bd5b7fd5962da000f8d421ac4f16b3b65cf5920c
valentina Li <valentinalidev@gmail.com>
2024-04-29 19:25:09 +0200

    fixed heigth navbar

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Root } from "./Root";
+import { NoContainer } from "./NoContainer";
 
 createRoot(document.getElementById('root')).render(
     <Root/>
