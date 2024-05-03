73a1b2e0b37fe9d40d56d6d5d07d58222873ea20
valentina Li <valentinalidev@gmail.com>
2024-05-03 17:19:31 +0200

    fixed faq

--- src/components/faq/AccordionFaq.jsx
+++ src/components/faq/AccordionFaq.jsx
@@ -1,19 +1,15 @@
 export const AccordionFaq = [
     {
         question: 'Come contattarci?',
-        answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt doloribus iure nemo ad ex perspiciatis, veritatis aliquid minus accusamus!'
+        answer: "È possibile contattarci direttamente tramite email all'indirizzo pixelplayground@gmail.com o puoi scriverci in DM su Instagram o Facebook!"
 },
 {
     question: 'Come effettuare un ordine?',
-    answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt doloribus iure nemo ad ex perspiciatis, veritatis aliquid minus accusamus!'
+    answer: "Naviga sul sito per trovare i giochi che desideri acquistare. Aggiungili al carrello. Se non hai già un account sul sito, crea un account inserendo le informazioni richieste. Una volta selezionati i giochi, vai al carrello per visualizzare gli articoli selezionati e il totale dell'ordine. Inserisci l'indirizzo di spedizione corretto in modo che gli articoli possano essere recapitati correttamente. Seleziona il metodo di pagamento preferito tra le opzioni disponibili. Prima di confermare l'ordine, controlla attentamente il riepilogo dell'ordine per assicurarti che tutti gli articoli e i dettagli siano corretti. Una volta verificato tutto, conferma l'ordine. Riceverai una conferma dell'acquisto via email.    "
 },
 {
     question: 'Come posso pagare?',
-    answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt doloribus iure nemo ad ex perspiciatis, veritatis aliquid minus accusamus!'
-},
-{
-    question: 'Supporto tecnico',
-    answer: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo incidunt doloribus iure nemo ad ex perspiciatis, veritatis aliquid minus accusamus!'
+    answer: 'Attualmente è possibile pagare con Carta di debito a circuito Mastercar e Visa, Paypal e Google Pay'
 },
 {
     question: 'Reso e rimborsi',
